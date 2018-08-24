b5d927542597638cd975d5ee5185eaa65ff27e55
Adam Peter Banham <n7176546@qut.edu.au>
2018-08-24 13:53:04 +1000

    Updated segment to follow prac guidlines

--- Segment.m
+++ Segment.m
@@ -1,43 +1,91 @@
-function [ ReturnImage , blobs ] = Segment ( ImageLocation )
+function [ ReturnImage , blobs ] = Segment ( WorkSheet  )
 %Segment loads a image and returns segment or highlights sections of a
 %image
 %   ReturnImage is the final image after image processing is handled
 %   blobs is the image segmentation object returned after processing
-im = imread(ImageLocation);
-
-im2 = imresize(im, 0.2);
-im = im2;
-
-colourThershold = .6;
+imWork = imread(WorkSheet);
+imTest = imWork(1:400,:,:);
+imWork = imWork(400:1600,:,:);
+colourThershold = .5;
 Colours = ["Red","Green","Blue"];
-%show original image
-figure(2)
-idisp(im);
-%Apply chromacticity to test pixels
-[ chromacity ] = Chromactiy( im , colourThershold );
-%use chromacity images to form blobs.[colour] struct for blobs found of
-%that colour
+Q = [20 380; 200 380; 380 380; 20 200; 200 200; 200 20; 20 20; 380 200; 380 20];
+%Pract Exam Format
+%find bright colours on test sheet
+[ chroTest ] = Chromactiy( imTest , colourThershold );
+%display test sheet
+idisp(imTest);
+%for each colour find shapes and return bounding box if found
+% 'area',[55646,2258168]
 for i = 1:3
-    blobs.(Colours(i)) = iblobs(chromacity(:,:,i)>colourThershold ,[5000,3000], 'boundary');
+    testBlobs.(Colours(i)) = iblobs(chroTest(:,:,i)>colourThershold,'area',[3000,22000], 'boundary');
+    for j = 1:length(testBlobs.(Colours(i)))
+        testBlobs.(Colours(i))(j).plot_box('b');
+        testBlobs.(Colours(i))(j).plot('r*');
+    end  
 end
-%show each boundary box for blue blobs
-%idisp(chromacity(:,:,3)>colourThershold);
-blobs.Blue.plot_box('g');
-blobs.Blue.plot('r*');
+disp('finished with test sheet');
 pause;
-%sort coloured blobs into shapes.[shapetype].[colour] struct
-[ Shapes ] = Circularity( blobs );
-%display the largest red triangle
-figure(3)
-%display original image
-idisp(im);
-hold on;
-%find the big red triagnle
-[~ , idx] = max(Shapes.Triangle.Red.area);
-%plot boundary box
-Shapes.Triangle.Red(idx).plot_box();
+%get a binary image of calibration marks
+[ chroWork ] = Chromactiy( imWork , colourThershold );
+idisp(chroWork(:,:,3) > colourThershold);
+disp('Segmented the blue calibration marks on the work sheet');
+pause;
+%display binary image of all other shapes
+redShapes = chroWork(:,:,1) > colourThershold;
+greenShapes = chroWork(:,:,2) > colourThershold;
+allShapes = redShapes | greenShapes;
+idisp(allShapes);
+disp('binary image of all other shapes');
+pause;
+blobs = iblobs(allShapes,'boundary');
+blobs = blobs(2:end);
+blobs.plot('r*');
+pause;
+idx = find(blobs.circularity < .71 & blobs.circularity > .6 );
+blobs(idx).plot_box('b');
+pause;
 
-%return the final image
-ReturnImage = im;
+
+% %show original image
+% figure(2)
+% idisp(imWork);
+% %Apply chromacticity to test pixels
+% [ chromacity ] = Chromactiy( imWork , colourThershold );
+% %use chromacity images to form blobs.[colour] struct for blobs found of
+% %that colour
+% for i = 1:3
+%     blobs.(Colours(i)) = iblobs(chromacity(:,:,i)>colourThershold,'area',[3000,22000], 'boundary');
+%     
+% end
+% 
+% %show each boundary box for blue blobs
+% %idisp(chromacity(:,:,3)>colourThershold);
+% % % idisp(im)
+% % % blobs.Blue.plot_box('b');
+% % % blobs.Blue.plot('b*');
+% % % pause;
+% % % idisp(im)
+% % % blobs.Red.plot_box('r');
+% % % blobs.Red.plot('r*');
+% % % pause;
+% % % idisp(im)
+% % % blobs.Green.plot_box('g');
+% % % blobs.Green.plot('g*');
+% % % pause;
+% 
+% %sort coloured blobs into shapes.[shapetype].[colour] struct
+% [ Shapes ] = Circularity( blobs );
+% %display the largest red triangle
+% figure(3)
+% %display original image
+% idisp(imWork);
+% hold on;
+% %find the big red triagnle
+% [~ , idx] = max(Shapes.Triangle.Red.area);
+% %plot boundary box
+% Shapes.Triangle.Red(idx).plot_box();
+% 
+% %return the final image
+ReturnImage = imWork;
 end
 
