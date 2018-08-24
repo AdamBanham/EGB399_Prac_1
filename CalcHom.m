4d7a447f6646f56b81e0f689e891bee8822883e1
Jonathon O'Neill <jonnyo98@hotmail.com>
2018-08-24 13:51:20 +1000

    Reorder blobs in homography calc

--- CalcHom.m
+++ CalcHom.m
@@ -1,5 +1,5 @@
 function H = CalcHom(BlueBlobs)
-    Q = [20 380; 200 380; 380 380; 20 200; 200 200; 200 20; 20 20; 380 200; 380 20];
+    Q = [20 380; 200 380; 380 380; 20 200; 200 200; 380 200; 20 20; 200 20; 380 20];
     Pb = Q;
     index = 2; % Exclude background, as background is at position 1.
     for i = BlueBlobs
